8937b97d08e8cafa416139d02dbf0abc4495197a
ShenPrime <splinteredsanity@outlook.com>
2024-03-29 19:59:32 +0100

    Added: hover tooltip for copy button

--- lib/electro_db_web/live/home_page/components.ex
+++ lib/electro_db_web/live/home_page/components.ex
@@ -3,17 +3,18 @@ defmodule ElectroDbWeb.HomePage.Components do
 
   def copy_button(assigns) do
     ~H"""
-    <div class="flex justify-center">
+    <div class="flex justify-center text-center">
       <button
-        class="w-6 mr-2"
+        class="mr-2 flex justify-center text-center"
         id={"copy-#{@column}-#{@id}"}
         phx-hook="Copy"
         data-to={"##{@column}-#{@id}"}
         phx-click="copy-message-success"
+        title="Copy to clipboard"
       >
         📋
+        <h2 class="font-bold text-sky-400 text-center hover:text-sky-300"><%= @label %>:</h2>
       </button>
-      <h2 class="font-bold text-sky-400"><%= @label %>:</h2>
     </div>
     <p id={"#{@column}-#{@id}"} class="m-1"><%= @content %></p>
     """
